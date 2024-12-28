45bac1da8c7baeaa2ba24198f6676fa40795dd7f
saim khan <salim.khan@mobulous.com>
2024-12-28 23:32:44 +0530

    configure socket

--- SocketConnection/Socket.js
+++ SocketConnection/Socket.js
@@ -4,7 +4,7 @@ const { Server } = require("socket.io");
 const createSocketServer = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["https://salimchat.netlify.app","http://localhost:5173"],
       credentials: true,
       methods: ["GET", "POST", "PUT", "DELETE"],
     },
